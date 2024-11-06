62b2f59574b942f098880f47aeea90d36ff207be
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-11-06 19:29:13 +0200

    feat(src): added animation preloader + update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Footer from "./sections/Footer";
+import PreLoader from "./components/PreLoader";
+// import { useState } from "react";
 
 const App = () => {
+
   return (
     <main className="max-w-7xl mx-auto relative">
+    
+    <PreLoader />
+
       <Navbar />
       <Hero />
       <About />
       <Projects />
-      {/* <Contact /> */}
       <Footer/>
     </main>
   );
